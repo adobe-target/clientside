2fc4ae2d90769dade95cf924925a5bca0439c3d1
Nina Ciocanu <nciocanu@adobe.com>
2018-03-20 01:59:29 +0200

    added react-router-redux and subscribed to history changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Routes from './Routes';
 import productsData from './data/products';
 import cartReducer from './controllers/cart';
 import productsReducer from './controllers/products';
 import {Provider} from 'react-redux';
 import {combineReducers, createStore} from 'redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import {Route, Router, browserHistory, Redirect, hashHistory} from 'react-router'
+import {Left, Whoops404} from "./components";
+import CheckoutPage from "./pages/CheckoutPage";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import TransformerList from "./pages/TransformerList";
 
 const rootReducer = combineReducers({
     cart: cartReducer,
-    products: productsReducer
+    products: productsReducer,
+    routing: routerReducer
 });
 
 let store = createStore(
@@ -19,12 +26,30 @@ let store = createStore(
     },
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // for debugging
 );
+const history = syncHistoryWithStore(hashHistory, store);
+
+history.listen(location => {
+
+    var event = new CustomEvent('at-view-start', { detail: location.pathname });
+
+    document.dispatchEvent(event);
+    console.log("changed location event was dispatched", event)
+});
 
 window.React = React
 
 render(
     <Provider store={store}>
-        <Routes/>
+        <Router history={history}>
+            <Redirect from="/" to="home"></Redirect>
+            <Route path="/" component={Left}>
+                <Route path="/home" component={Home}/>
+                <Route path="/transformers" component={TransformerList}/>
+                <Route path="/products" component={Products}/>
+                <Route path="/cart" component={CheckoutPage}/>
+            </Route>
+            <Route path="*" component={Whoops404}/>
+        </Router>
     </Provider>
 
     ,
